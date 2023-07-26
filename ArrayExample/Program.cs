4ef8afae331adb430ca221cc41d3b9523ee2a21a
Misbahul Islam <imisbahul53@gmail.com>
2023-07-26 23:32:49 +0600

    first commit

---
+++ ArrayExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] nubers = new int[5] ;
+            nubers[0]= 34;
+            nubers[1] = 56;
+            for (int i = 0; i < nubers.Count(); i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CsharfExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CsharfExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result= Add("misba ", "islam");
+            //foreach( var res in result )
+            
+            Console.WriteLine (result);
+            Console.ReadKey();
+            
+        }
+        static string Add (string FirstName, string LastName)
+        {
+            string sum = FirstName + LastName;
+            return sum;
+        }
+    }
+}
